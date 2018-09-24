8c7db8e0d5a225ab9c50e78ac9efae61ebd5b5f2
Raydel Miranda <raydel.miranda.gomez@gmail.com>
2018-09-24 13:25:16 -0400

    Fixing a fixture (it was no compatible with django2.0)

--- download/tests/conftest.py
+++ download/tests/conftest.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import UserManager
 
 @pytest.fixture(scope='session')
 def test_user():
-    user = UserManager().create_user(
+    user_manager = UserManager()
+    user = user_manager.create_user(
         'test', email='test@test.com', password='test.pass.123'
     )
     return user
